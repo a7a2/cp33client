dacebeda71e8d8d9dfe35f7006b9db44bab0e64a
waa hong <waahong@waas-MacBook-Pro.local>
2017-08-21 14:30:56 +0800

    移除数据库方式获取数据

--- ready.go
+++ ready.go
@@ -12,30 +12,30 @@ import (
 )
 
 func getCron(gameId int) {
-	dt := dataTime{}
-	err := Db.Model(&dt).Where("type=? and action_time<?", gameId, time.Now().Format("15:04:05")).Order("action_time DESC").Limit(1).Select()
-	if err == nil && dt.Type >= 0 {
-		//fmt.Println("getCron() 20:", dt.ActionTime)
-	} else if err != nil {
-		fmt.Println("getCron() 28:" + err.Error())
-		return
-	}
-	//fmt.Println(dt.ActionTime)
-	var ttActionTime time.Time
-	ttActionTime, err = time.ParseInLocation("2006-01-02 15:04:05", time.Now().Format("2006-01-02")+" "+dt.ActionTime, time.Local)
-	if err != nil {
-		fmt.Println("getCron() 29:", err.Error())
-		return
-	}
-	diffTime := time.Now().Local().Unix() - ttActionTime.Unix()
-	if diffTime > 120 || diffTime < -15 { //2分钟采集不到就别浪费时间了
-		//fmt.Println("getCron() 34:"+"不在采集时间内跳过！", dt.Type, "	", diffTime)
-		return
-	}
-
+	//	dt := dataTime{}
+	//	err := Db.Model(&dt).Where("type=? and action_time<?", gameId, time.Now().Format("15:04:05")).Order("action_time DESC").Limit(1).Select()
+	//	if err == nil && dt.Type >= 0 {
+	//		//fmt.Println("getCron() 20:", dt.ActionTime)
+	//	} else if err != nil {
+	//		fmt.Println("getCron() 28:" + err.Error())
+	//		return
+	//	}
+	//	//fmt.Println(dt.ActionTime)
+	//	var ttActionTime time.Time
+	//	ttActionTime, err = time.ParseInLocation("2006-01-02 15:04:05", time.Now().Format("2006-01-02")+" "+dt.ActionTime, time.Local)
+	//	if err != nil {
+	//		fmt.Println("getCron() 29:", err.Error())
+	//		return
+	//	}
+	//	diffTime := time.Now().Local().Unix() - ttActionTime.Unix()
+	//	if diffTime > 120 || diffTime < -15 { //2分钟采集不到就别浪费时间了
+	//		//fmt.Println("getCron() 34:"+"不在采集时间内跳过！", dt.Type, "	", diffTime)
+	//		return
+	//	}
+	var err error
 	var resp *http.Response
 	resp, err = surfer.Download(&surfer.Request{
-		Url: "http://127.0.0.1:8080/apiMyself/" + strconv.Itoa(dt.Type),
+		Url: "http://127.0.0.1:8080/apiMyself/" + "1",
 		//DownloaderID: 1,
 	})
 	if err != nil {
@@ -54,6 +54,9 @@ func getCron(gameId int) {
 	err = json.Unmarshal(b, &openInfo)
 	if err != nil {
 		fmt.Println("getCron()61:", err.Error())
+		return
+	}
+	if openInfo.Last_open != "" {
 		return
 	}
 	//fmt.Println("getCron()64:", openInfo.Last_period, "	", diffTime)
